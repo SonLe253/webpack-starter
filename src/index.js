6c48554ad93ce88e4cc0b3e6044ac67aa2047f2f
Son Le <son.le@salesforce.com>
2019-06-18 14:45:10 -0400

    Working with ES6+ and Typescript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('This is index JS');
+import { Header } from './app/header';
+import { Footer } from './app/footer';
+
+let header = new Header();
+let firstHeading = header.getFirstHeading();
+
+let footer = new Footer();
+let footerText= footer.getFooterText();
+
+console.log(firstHeading);
+console.log(footerText);
